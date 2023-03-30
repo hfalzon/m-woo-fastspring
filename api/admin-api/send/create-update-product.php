847b6e56a87ae5165267fdb22ead81a950bb1aff
Hayden Falzon <hfalcon.film@gmail.com>
2023-03-30 15:52:28 +1100

    update - create/update product supports subs

--- api/admin-api/send/create-update-product.php
+++ api/admin-api/send/create-update-product.php
@@ -4,6 +4,7 @@ add_action('woocommerce_new_product', 'mwfi_create_update_product', 10, 2); // A
 add_action('woocommerce_update_product', 'mwfi_create_update_product', 10, 2);
 function mwfi_create_update_product( $id, $product )
 {
+    //wp_die( var_dump( $_POST ) ); //Debug
     /**Testing */
     $username = get_option('mwfi_api_key');
     $password = mwfi_decrypt( get_option('mwfi_api_secret_key') );
@@ -15,6 +16,25 @@ function mwfi_create_update_product( $id, $product )
     }
     $taxcode = $_POST['mwfi_product_taxcode'];
     $format = new mwfi_product_data( $_POST['mwfi_product_type'] );
+    //Create pricing array
+    $pricing = array(
+        'quantityBehavior' => 'allow',
+        'quantityDefault' => 1,
+        'price' => array(
+            get_woocommerce_currency() => $product -> get_price()
+        ),
+    );
+    //Add subscription period if subscription to pricing
+    if ( $_POST['mwfi_product_subscription'] == 1 )
+    {
+        $accpeted_intervals = array('day', 'week', 'month', 'year');
+        //Validate interval
+        if ( in_array( $_POST['mwfi_product_subscription_interval'], $accpeted_intervals ) ) 
+        {
+            $pricing['interval'] = sanitize_text_field( $_POST['mwfi_product_subscription_interval'] );
+            $pricing['intervalLength'] = absint( $_POST['mwfi_product_subscription_length'] );
+        }
+    }
     //Create product
     $data = array( 
         'products' => array(
@@ -32,12 +52,7 @@ function mwfi_create_update_product( $id, $product )
             ),
             'format' => $format -> format,
             'taxcode' => $taxcode,
-            'pricing' => array(
-                'quantityBehabior' => 'single',
-                'price' => array(
-                    get_woocommerce_currency() => $product -> get_price()
-                )
-            )
+            'pricing' => $pricing,
         )
     );
     //wp_die(var_dump($format));
@@ -83,47 +98,58 @@ function mwfi_create_update_product( $id, $product )
         $query = "SELECT fs_product_path FROM $table_name WHERE wc_product_id = '%d'";
         $query = $wpdb->prepare($query, $id);
         $product_path = $wpdb->get_var($query);
-
+        //Create base array
+        $update = array(
+            'fs_taxcode' => sanitize_text_field( $taxcode ),
+            'fs_product_type' => $format -> id,
+        );
+        $update_format = array(
+            '%s',
+            '%d',
+        );
+        //Check if product is subscription
+        if ( $_POST['mwfi_product_subscription'] == 1 ) //TODO LOGIC
+        {
+            //Add subscription to update array
+            $sub_array = array(
+                'fs_product_subscription' => 1,
+                'fs_product_subscription_interval' => sanitize_text_field( $_POST['mwfi_product_subscription_interval'] ),
+                'fs_product_subscription_interval_length' => absint( $_POST['mwfi_product_subscription_length'] ),
+            );
+            $update = array_merge($update, $sub_array);
+            array_push($update_format, '%d', '%s', '%d');
+        }
+        else
+        {
+            //Add subscription to update array
+            $update['fs_subscription'] = 0;
+            array_push($update_format, '%d');
+        }
         //If path exists check if it needs to be updated
         if ($product_path)
         {
+            //Needs to be updated
+            $where = array(
+                'wc_product_id' => $id,
+            );
+            $where_format = array(
+                '%d',
+            );
             //Check if product path needs to be updated
             if ($product_path != $product_id)
             {
-                //Update product path
-                $wpdb->update(
-                    $table_name,
-                    array( 
-                        'fs_product_path' => $product_id,
-                        'fs_taxcode' => $taxcode,
-                        'fs_product_type' => $format -> id
-                    ),
-                    array( 'wc_product_id' => $id ),
-                    array( 
-                        '%s',
-                        '%s',
-                        '%d'
-                    ),
-                    array( '%d' )
-                );
+                //Add product path to update array
+                $update['fs_product_path'] = $product_id;
+                array_push($update_format, '%s');
             }
-            else
-            {
-                //Update taxcode and product type only
-                $wpdb->update(
-                    $table_name,
-                    array( 
-                        'fs_taxcode' => $taxcode,
-                        'fs_product_type' => $format -> id
-                    ),
-                    array( 'wc_product_id' => $id ),
-                    array( 
-                        '%s',
-                        '%d'
-                    ),
-                    array( '%d' )
-                );
-            }
+            //Update product
+            $wpdb -> update(
+                $table_name,
+                $update,
+                $where,
+                $update_format,
+                $where_format
+            );
         }
         else
         {
