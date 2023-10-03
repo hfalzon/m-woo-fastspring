79709e0474ada69878708c068ef343bb91efd002
Hayden Falzon <hfalcon.film@gmail.com>
2023-10-03 18:52:54 +1100

    update - test 1 one more

--- frontend/checkout.php
+++ frontend/checkout.php
@@ -12,6 +12,7 @@ add_action('woocommerce_checkout_process', 'mwfi_handle_checkout');
 
 add_action('wp_head', 'mwfi_fs_checkout');
 //TODO: make a setting that allows the user to choose the data-storefront
+//TODO: 
 function mwfi_fs_checkout()
 {
     ?>
@@ -19,7 +20,7 @@ function mwfi_fs_checkout()
             id="fsc-api"
             src="https://sbl.onfastspring.com/sbl/0.9.5/fastspring-builder.min.js"
             type="text/javascript"
-            data-storefront="blendertutorials.onfastspring.com/popup-blendertutorials"
+            data-storefront="blendertutorials.test.onfastspring.com/popup-blendertutorials"
             data-popup-webhook-received="dataPopupWebhookReceived"
             data-popup-closed="dataPopupClosed"
             data-error-callback="dataErrorCallback"
